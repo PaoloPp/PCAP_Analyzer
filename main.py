50d7855f97fecba8e86aa1b2f39a6c6e633fa198
Paolo Palmiero <paolopalmiero@Paolos-MacBook-Air.local>
2024-10-04 17:14:13 +0200

    Added the extract_dns_pkt function

--- main.py
+++ main.py
@@ -13,46 +13,32 @@ def main():
     print("Pcap directory: " + c.PCAP_DIR)
     print("Scapy Version: " + str(scapy.__version__))
     print("Python Version: " + str(sys.version))
-    extract_pcap("28_06_1000-1330.pcap")
 
-#def send_email():
-#    sender_email = 'paolopalmiero.p@gmail.com'
-#    receiver_email = 'paolopalmiero.p@gmail.com'
-#    subject = 'Extraction complete'
-#    body = 'Congratulations! The extraction process has been completed successfully.'
-#    
-#    # Create the email message
-#    msg = MIMEMultipart()
-#    msg['From'] = sender_email
-#    msg['To'] = receiver_email
-#    msg['Subject'] = subject
-#    msg.attach(MIMEText(body, 'plain'))
-#    
-#    # SMTP server configuration
-#    smtp_server = 'smtp.gmail.com'
-#    smtp_port = 587
-#    smtp_user = 'your_email@example.com'
-#    smtp_password = 'your_password'
-#    
-#    try:
-#        # Create a secure connection with the server
-#        server = smtplib.SMTP(smtp_server, smtp_port)
-#        server.starttls()  # Upgrade the connection to a secure encrypted SSL/TLS connection
-#    
-#        # Log in to the email account
-#        server.login(smtp_user, smtp_password)
-#    
-#        # Send the email
-#        server.send_message(msg)
-#    
-#        print('Email sent successfully.')
-#    
-#    except Exception as e:
-#        print(f'Error: {e}')
-#    
-#    finally:
-#        # Terminate the SMTP session and close the connection
-#        server.quit()
+def extract_dns_pkt():
+
+    dns_pkt = []
+
+    write_file = PcapWriter(c.PCAP_DIR + "dns_pkts" + ".pcap", append=True)
+
+    for i in range(1,52):
+
+        directory = c.PCAP_DIR + c.IP_PREFIX + str(i) + "/"
+
+        if os.path.isdir(directory):
+            for filename in os.listdir(directory):
+                f = os.path.join(directory, filename)
+                print(f)
+                if os.path.isfile(f):
+                    try:
+                        current_pcap = open_pcap(f)
+                    except NameError:
+                        print("Error: current_pcap is not defined.")
+                    print("Extracting DNS packets from: " + f)
+                    for packet in current_pcap:
+                        if packet.haslayer(DNS):
+                            dns_pkt.append(packet)
+
+    write_file.write(dns_pkt)
 
 
 def extract_pcap(file_name):    
@@ -75,14 +61,14 @@ def extract_pcap(file_name):
             else:
                 print("Directory already exists.")
 
-            write_file = PcapWriter(c.PCAP_DIR + current_ip + "/" + file_name + "_" + current_ip + ".pcap", append=True)
-
+            write_file = PcapWriter(c.PCAP_DIR + current_ip + "/" + current_ip + "_" + file_name, append=True)
+            print(write_file)
             for packet in current_cap:
                 if packet.haslayer(IP):
                     if packet[IP].src == current_ip or packet[IP].dst == current_ip:
                         print(packet.summary())
                         filtered_pkts.append(packet)
-            write_file.write(packet)
+            write_file.write(filtered_pkts)
     
 #
 #        wrpcap(c.PCAP_DIR + current_ip +
@@ -92,7 +78,7 @@ def extract_pcap(file_name):
 
 def open_pcap(name):
     print("Opening PCAP file: " + name)
-    cap = PcapReader(c.PCAP_DIR + name)
+    cap = PcapReader(name)
     return cap
 
 
